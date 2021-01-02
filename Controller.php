e402873d2008d42f236d3a131104911afe8db120
samueloliva <samuel.zanferdinioliva@gmail.com>
2021-01-02 16:14:13 -0300

    Creating Microwave class

---
+++ Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("ElectronicItem.php");
+
+/**
+ * Leaf - primitive type
+ */
+class Controller extends ElectronicItem 
+{
+    public function __construct($price, $wired) {
+        $this->setType(self::ELECTRONIC_ITEM_CONTROLLER);
+        $this->setPrice($price);
+        $this->setWired($wired);
+    }
+
+    /**
+     * Returns number max of extra items
+     * @return int
+     */
+    public function maxExtras() {
+        // Controller can't have any extras
+        return 0;
+    }
+    
+}
+
+?>
